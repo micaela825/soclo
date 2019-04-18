eb7bc4657f248404fca1a27a0da8618fadd08263
Micaela O'Herron <micaela.oherron@gmail.com>
2019-04-18 07:25:54 -0400

    styling

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
+  <div className="navBar">
     <nav>
       <div>
         <Link to="/home" id="logo">
