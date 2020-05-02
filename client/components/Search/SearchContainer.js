195df1e885eed1e6b3823f9f1a198075ae05c5dd
Micaela O'Herron <micaela.oherron@gmail.com>
2020-05-01 23:04:43 -0400

    Revert "Micaela/code cleanup"

--- client/components/Search/SearchContainer.js
+++ client/components/Search/SearchContainer.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 
-class Search extends Component {
+class SearchContainer extends Component {
   render() {
     return (
       <div>
@@ -10,4 +10,4 @@ class Search extends Component {
   }
 }
 
-export default Search
+export default SearchContainer
