dc08ac36f489d020badb84923a6a573f425e2ffa
Micaela O'Herron <micaela.oherron@gmail.com>
2020-05-06 12:11:59 -0400

    feat auth validation on create account

--- client/components/Auth/index.js
+++ client/components/Auth/index.js
@@ -1,86 +1,93 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import PropTypes from 'prop-types'
 import {auth} from '../../store'
 import './index.scss'
 import Icon from '../../../public/icon'
-import isEmailValid from './utils/isEmailValid'
-import isPasswordValid from './utils/isPasswordValid'
+// import isEmailValid from './utils/isEmailValid'
+// import isPasswordValid from './utils/isPasswordValid'
 
 const BASE_CLASS = 'auth'
 
-const AuthForm = props => {
-  const {name, displayName, handleSubmit, error, signUpError} = props
+class AuthForm extends Component {
+  constructor(props) {
+    super(props)
 
-  const alternativeFormName = () => {
+    this.alternativeFormName = this.alternativeFormName.bind(this)
+    this.alternativeRoute = this.alternativeRoute.bind(this)
+    this.err = ''
+  }
+
+  alternativeFormName = name => {
     return name == 'signup' ? 'Sign in' : 'Sign up'
   }
 
-  const alternativeRoute = () => {
+  alternativeRoute = name => {
     return name == 'signup' ? 'login' : 'signup'
   }
 
-  return (
-    <div className={BASE_CLASS}>
-      <Icon className="icon" />
-      <div className={`${BASE_CLASS}__title`}>social closet</div>
-      <form
-        onSubmit={handleSubmit}
-        name={name}
-        className={`${BASE_CLASS}__form`}
-      >
-        {name === 'signup' && (
+  render() {
+    return (
+      <div className={BASE_CLASS}>
+        <Icon className="icon" />
+        <div className={`${BASE_CLASS}__title`}>social closet</div>
+        <form
+          onSubmit={this.props.handleSubmit}
+          name={this.props.name}
+          className={`${BASE_CLASS}__form`}
+        >
+          {this.props.name === 'signup' && (
+            <input
+              name="userName"
+              type="text"
+              placeholder="your name"
+              className={`${BASE_CLASS}__form__input`}
+            />
+          )}
           <input
-            name="userName"
+            name="email"
             type="text"
-            placeholder="your name"
+            placeholder="email"
             className={`${BASE_CLASS}__form__input`}
           />
-        )}
-        <input
-          name="email"
-          type="text"
-          placeholder="email"
-          className={`${BASE_CLASS}__form__input`}
-        />
-        <input
-          name="password"
-          placeholder="password"
-          type="password"
-          className={`${BASE_CLASS}__form__input`}
-        />
+          <input
+            name="password"
+            placeholder="password"
+            type="password"
+            className={`${BASE_CLASS}__form__input`}
+          />
 
-        <button type="submit" className={`${BASE_CLASS}__form__submit`}>
-          {displayName}
-        </button>
+          <button type="submit" className={`${BASE_CLASS}__form__submit`}>
+            {this.props.displayName}
+          </button>
+          {this.err}
+          {this.props.error &&
+            this.props.error.response && (
+              <div className={`${BASE_CLASS}__form__error`}>
+                {this.props.error.response.data}
+              </div>
+            )}
+        </form>
+        <a href="/auth/google" className={`${BASE_CLASS}__socialauth`}>
+          {this.props.displayName} with Google
+        </a>
+        <div className={`${BASE_CLASS}__or`}>or</div>
 
-        {error &&
-          error.response && (
-            <div className={`${BASE_CLASS}__form__error`}>
-              {' '}
-              {error.response.data}{' '}
-            </div>
-          )}
-        {signUpError && <div>{signUpError}</div>}
-      </form>
-      <a href="/auth/google" className={`${BASE_CLASS}__socialauth`}>
-        {displayName} with Google
-      </a>
-      <div className={`${BASE_CLASS}__or`}>or</div>
-
-      <a href={`/${alternativeRoute()}`} className={`${BASE_CLASS}__route`}>
-        {alternativeFormName()}
-      </a>
-    </div>
-  )
+        <a
+          href={`/${this.alternativeRoute(this.props.name)}`}
+          className={`${BASE_CLASS}__route`}
+        >
+          {this.alternativeFormName(this.props.name)}
+        </a>
+      </div>
+    )
+  }
 }
 
 const mapLogin = state => {
   return {
     name: 'login',
     displayName: 'Sign in',
-    error: state.user.user.error,
-    signUpError: ''
+    error: state.user.user.error
   }
 }
 
@@ -88,8 +95,7 @@ const mapSignup = state => {
   return {
     name: 'signup',
     displayName: 'Sign up',
-    error: state.user.user.error,
-    signUpError: ''
+    error: state.user.user.error
   }
 }
 
@@ -97,36 +103,14 @@ const mapDispatch = dispatch => {
   return {
     handleSubmit(evt) {
       evt.preventDefault()
-
       const formName = evt.target.name
       const email = evt.target.email.value
       const password = evt.target.password.value
       const userName = evt.target.userName ? evt.target.userName.value : ''
-      if (email && !isEmailValid(email)) {
-        console.log(
-          'todo - make component stateful & add sign up error on local state'
-        )
-      } else if (password && !isPasswordValid) {
-        console.log(
-          'todo - make component stateful & add sign up error on local state'
-        )
-      } else {
-        dispatch(auth(userName, email, password, formName))
-      }
+      dispatch(auth(userName, email, password, formName))
     }
   }
 }
 
 export const Login = connect(mapLogin, mapDispatch)(AuthForm)
 export const Signup = connect(mapSignup, mapDispatch)(AuthForm)
-
-/**
- * PROP TYPES
- */
-AuthForm.propTypes = {
-  name: PropTypes.string.isRequired,
-  displayName: PropTypes.string.isRequired,
-  handleSubmit: PropTypes.func.isRequired,
-  error: PropTypes.object,
-  signUpError: PropTypes.string
-}
